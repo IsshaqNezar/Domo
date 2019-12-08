0beb1df9499965e53c5410830b9f3e0d6130b076
IsshaqNezar <Isshaq1412@hotmail.fr>
2019-12-08 18:32:06 +0100

    2

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const bodyParser = require ('body-parser');
 
 const app = express();
 
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
+const port = 5000;
+
+io.on("connection", socket => {
+    console.log("a user is connected");
+    console.log(socket.id);
+});
+
+server.listen(port, () => console.log("server running on port" + port));
+
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(bodyParser.json())
